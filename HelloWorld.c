7dba804b42a5ba2069ed96bf2a0a6f8d834117cf
Angelo Pumar <angelopumar987@gmail.com>
2024-09-02 23:00:16 +0800

    Added Battle Announcement

--- HelloWorld.c
+++ HelloWorld.c
@@ -12,4 +12,8 @@ int main() {
     printf("Player 2 name: (enter your name)\n");
     scanf("%s", game_name2);
     printf("Welcome to the game world %s!\n", game_name2);
+
+    printf("Prepare yourselves, %s and %s! The battle is about to begin!\n", game_name, game_name2);
+
+    return 0;
 }
